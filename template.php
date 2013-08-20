870cefb8932dc718aff12f0291639616f1129529
Andreas Sahle <andreas.sahle@wunderkraut.com>
2013-08-20 17:34:01 +0200

    add node template and preprocessing

--- template.php
+++ template.php
@@ -17,7 +17,6 @@ function nameless_preprocess_html(&$variables, $hook) {
 
   $variables['add_responsive_meta'] = TRUE;
 
-
   // Attributes for html element.
   $variables['html_attributes_array'] = array(
     'lang' => $variables['language']->language,
@@ -36,4 +35,38 @@ function nameless_preprocess_html(&$variables, $hook) {
 function nameless_process_html(&$variables, $hook) {
   // Flatten out html_attributes.
   $variables['html_attributes'] = drupal_attributes($variables['html_attributes_array']);
+}
+
+/**
+ * Override or insert variables into the page template.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("page" in this case.)
+ */
+function nameless_preprocess_page(&$variables, $hook) {
+  // remove default message from main content if there is no content created yet.
+  unset($variables['page']['content']['system_main']['default_message']);
+}
+
+/**
+ * Override or insert variables into the node templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("node" in this case.)
+ */
+function nameless_preprocess_node(&$variables, $hook) {
+  // Add $unpublished variable.
+  $variables['unpublished'] = (!$variables['status']) ? TRUE : FALSE;
+
+  // Add pubdate to submitted variable.
+  $variables['pubdate'] = '<time pubdate datetime="' . format_date($variables['node']->created, 'custom', 'c') . '">' . $variables['date'] . '</time>';
+  if ($variables['display_submitted']) {
+    $variables['submitted'] = t('Submitted by !username on !datetime', array('!username' => $variables['name'], '!datetime' => $variables['pubdate']));
+  }
+
+  $variables['title_attributes_array']['class'][] = 'node-title';
 }
