c15fa4de6d7c2a1757cfa181896c484227b86892
Benedikt Rötsch <info@benedikt-roetsch.de>
2013-09-16 12:13:22 +0200

    Changed block heading level from h4 to h2

--- templates/block--no-wrapper.tpl.php
+++ templates/block--no-wrapper.tpl.php
@@ -15,7 +15,7 @@
 
 <?php print render($title_prefix); ?>
 <?php if ($title): ?>
-  <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+  <h4<?php print $title_attributes; ?>><?php print $title; ?></h4>
 <?php endif; ?>
 <?php print render($title_suffix); ?>
 
