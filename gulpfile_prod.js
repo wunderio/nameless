6ba71ad6a28fc42c323b777a83f561c50f29489a
Andreas Sahle <andreas.sahle@wunderkraut.com>
2015-08-24 12:21:57 +0200

    try to install less dependencies for deployment

---
+++ gulpfile_prod.js
@@ -0,0 +1,25 @@
+/**
+ * Date: 29.01.15
+ * Time: 13:29
+ * @file Main gulp file for automation tasks on the production server
+ *
+ */
+
+'use strict';
+
+// Include Gulp & Tools We'll Use
+var gulp = require('gulp');
+
+require('gulp-help')(gulp, {
+  hideDepsMessage: true
+});
+
+// require (sub) tasks
+require('./gulp-tasks/tasks/style');
+
+// Because we are including gulp-help the syntax of a gulp task has an extra
+// description param at position 2 -
+// refer to https://www.npmjs.org/package/gulp-help
+
+// add your top gulp tasks here
+gulp.task('default', false, ['styles']);
